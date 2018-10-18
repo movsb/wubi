c4755787f50b3fd77d32c7444235ad3331937c8c
movsb <anhbk@qq.com>
2018-10-18 18:58:46 +0800

    86 wubi codes

--- src/main.go
+++ src/main.go
@@ -1,37 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"regexp"
+	"strings"
 )
 
 // Trie is a trie retrieval tree.
 type Trie struct {
-	chars map[rune]uint
+	words map[string]uint
 	nodes [26]*Trie
 }
 
-func (t *Trie) Insert(char rune, code string) {
-	tt := t
-	for _, c := range code {
-		if 'A' <= c && c <= 'Z' {
-			c += 32
+func (t *Trie) Insert(word string, codes ...string) {
+	for _, code := range codes {
+		tt := t
+		for _, c := range code {
+			if 'A' <= c && c <= 'Z' {
+				c += 32
+			}
+			if !('a' <= c && c <= 'z') {
+				panic("invalid code")
+			}
+			index := c - 'a'
+			if tt.nodes[index] == nil {
+				tt.nodes[index] = &Trie{}
+			}
+			tt = tt.nodes[index]
 		}
-		if !('a' <= c && c <= 'z') {
-			panic("invalid code")
+		if tt.words == nil {
+			tt.words = make(map[string]uint)
 		}
-		index := c - 'a'
-		if tt.nodes[index] == nil {
-			tt.nodes[index] = &Trie{}
-		}
-		tt = tt.nodes[index]
+		tt.words[word] = 1
 	}
-	if tt.chars == nil {
-		tt.chars = make(map[rune]uint)
-	}
-	tt.chars[char] = 1
 }
 
-func (t *Trie) Search(code string) map[rune]uint {
+func (t *Trie) Search(code string) map[string]uint {
 	tt := t
 	for _, c := range code {
 		if !('a' <= c && c <= 'z') {
@@ -39,16 +45,42 @@ func (t *Trie) Search(code string) map[rune]uint {
 		}
 		tt = tt.nodes[c-'a']
 		if tt == nil {
-			panic("no such code")
+			return nil
 		}
 	}
-	return tt.chars
+	return tt.words
+}
+
+var reWord = regexp.MustCompile(`^([^a-z]+)(.+)$`)
+
+func readCodes(file string, callback func(word string, codes ...string)) {
+	fp, err := os.Open(file)
+	if err != nil {
+		panic("error open")
+	}
+	defer fp.Close()
+	scanner := bufio.NewScanner(fp)
+	for scanner.Scan() {
+		line := scanner.Text()
+		tokens := reWord.FindStringSubmatch(line)
+		if len(tokens) < 3 {
+			panic("bad code")
+		}
+		word := tokens[1]
+		codes := strings.Split(tokens[2], " ")
+		callback(word, codes...)
+	}
 }
 
 func main() {
 	t := &Trie{}
-	t.Insert('我', "q")
-	t.Insert('人', "w")
-	t.Insert('多', "qq")
-	fmt.Println(t.Search("w"))
+	readCodes("../86", func(word string, codes ...string) {
+		t.Insert(word, codes...)
+	})
+
+	q := bufio.NewScanner(os.Stdin)
+	for q.Scan() {
+		words := t.Search(q.Text())
+		fmt.Println(words)
+	}
 }
